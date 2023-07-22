6b576dd489e7600a139555ef8bb9907f02368f01
Tendry24 <hei.tendry.2@gmail.com>
2023-07-22 18:09:50 +0300

    Feat : add second test on tests Folder

--- tests/hotelRoutes.test.js
+++ tests/hotelRoutes.test.js
@@ -6,13 +6,23 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe("Hotel Routes", () => {
-  it("should return all hotels", (done) => {
+    it("should return all hotels", (done) => {
     chai.request(app).get("/hotels")
       .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an("array");
-        expect(res.body).to.not.be.empty;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.not.be.empty;
         done();
       });
 });
+
+    it("should return one hotel by its id", (done) => {
+        const hotelId = 1;
+        chai.request(app).get(`/hotels/${hotelId}`)
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an("array").to.have.lengthOf(1);
+        done();
+      });
+  });
 });
