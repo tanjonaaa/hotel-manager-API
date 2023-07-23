e81a693d70c24a9a885de458e0dbd7075b14794b
tendryAxel <hei.tendry@gmail.com>
2023-07-23 17:48:13 +0300

    Fix : if not exists in update file

--- migration/update.sql
+++ migration/update.sql
@@ -2,7 +2,7 @@
 
 ALTER TABLE hotel ADD COLUMN IF NOT EXISTS photo varchar(300);
 ALTER TABLE room ADD COLUMN IF NOT EXISTS photo varchar(300);
-ALTER TABLE room ADD COLUMN description text default 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+ALTER TABLE room ADD COLUMN IF NOT EXISTS description text default 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Sed dicta commodi iusto fuga ipsam,
     reiciendis quidem consequuntur corporis,
     vero assumenda cum cupiditate architecto aspernatur,
