e0482a97e70316cfc6b5713b73fbdb5d8f6c467d
Tendry24 <hei.tendry.2@gmail.com>
2023-07-25 16:04:26 +0300

    Feat : Add new model

--- app/routes/hotelRoute.js
+++ app/routes/hotelRoute.js
@@ -8,6 +8,8 @@ export const hotelRouter = express.Router();
 
 hotelRouter.get('/', getAllHotels);
 
+hotelRouter.get('/hotel-by-city-ranking', (req, res) => getAllCity(req, res)); 
+
 hotelRouter .get("/rate", getAllHotelsOrderByRate);
 
 hotelRouter.get('/:id', getOneHotelByItsId);
