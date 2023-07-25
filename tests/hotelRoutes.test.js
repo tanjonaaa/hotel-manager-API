035696c1762c646911be068d577c8ba9cc462ba2
tanjonaaa <hei.tanjona@gmail.com>
2023-07-25 19:53:13 +0300

    feat: wrote tests for rate route and city route

--- tests/hotelRoutes.test.js
+++ tests/hotelRoutes.test.js
@@ -1,41 +1,62 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
-import { app } from "../index.js"; 
+import { app } from "../index.js";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe("Hotel Routes", () => {
     it("should return all hotels", (done) => {
-    chai.request(app).get("/hotels")
-      .end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res.body).to.be.an("array");
-            expect(res.body).to.not.be.empty;
-        done();
-      });
-});
-
-    it("should return one hotel by its id", (done) => {
-        const hotelId = 1;
-        chai.request(app).get(`/hotels/${hotelId}`)
-        .end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res.body).to.be.an("array").to.have.lengthOf(1);
-        done();
-      });
-  });
-
-    it("should return all hotels with their cities", (done) => {
-        chai.request(app).get('/hotels/withCities')
+        chai.request(app).get("/hotels")
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.an("array");
                 expect(res.body).to.not.be.empty;
-                res.body.forEach(e => {
-                    expect(e).to.have.property("city");
-                });
                 done();
-            })
-    })
+            });
+    });
+
+    it("should return one hotel by its id", (done) => {
+        const hotelId = 1;
+        chai.request(app).get(`/hotels/${hotelId}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an("array").to.have.lengthOf(1);
+                done();
+            });
+    });
+
+    it("should return all hotels with their cities", (done) => {
+        chai.request(app).get(`/hotels/hotel-by-city-ranking`)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an("array");
+                expect(res.body).to.not.be.empty;
+                
+                res.body.forEach(hotel => {
+                    expect(hotel).to.have.property('city_name');
+                });
+
+                done();
+            });
+    });
+
+    it("should return all hotels classed by average ratings", (done) => {
+        chai.request(app).get(`/hotels/rate`)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an("array");
+                expect(res.body).to.not.be.empty;
+                
+                res.body.forEach((hotel, index) => {
+                    if(index < res.body.length - 1){
+                        expect(
+                            parseFloat(hotel.total_rate)
+                        ).to.be.greaterThanOrEqual(parseFloat(res.body[index + 1].total_rate));
+                    }
+                });
+
+                done();
+            });
+    });
 });
