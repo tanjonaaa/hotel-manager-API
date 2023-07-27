a9106c8cf53bbeea44e651c989806971a2fccc2f
Tendry24 <hei.tendry.2@gmail.com>
2023-07-27 20:02:31 +0300

    Feat : Add UserRoute,controller and model

---
+++ app/controller/userController.js
@@ -0,0 +1,12 @@
+//Model methods imports
+import {
+    allUsers
+} from "../models/userModel.js"
+
+export const getAllUsers = (req,res)=>{
+    allUsers().then((results)=>{
+        res.status(200).json(results);
+    }).catch((e) => {
+        res.status(500).json({ error: "Failed to fetch AllCity." });
+    });
+}
