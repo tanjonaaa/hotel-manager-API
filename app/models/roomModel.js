ae2204c60aa45c8c1e6aa82c71bd396fdcec7191
tendryAxel <hei.tendry@gmail.com>
2023-07-28 19:02:05 +0300

    Feat : get all room availaible in a periode

--- app/models/roomModel.js
+++ app/models/roomModel.js
@@ -9,3 +9,21 @@ export const allRoom = async (id) => {
     `);
     return results.rows;
 }
+
+//Returns a Promise containing all room in specified periode
+export const allRoomAvailaible = async (start, end) => {
+    const results = await pool.query(`
+    SELECT room.*
+    FROM (
+        SELECT *FROM reservation
+            WHERE  '${start}'<=start_time AND start_time<='${end}'
+        UNION
+        SELECT * FROM reservation
+            WHERE  '${start}'<=end_time AND end_time<='${end}'
+    ) as indisponibility
+        RIGHT JOIN room
+        ON room.id = indisponibility.room_id
+        WHERE indisponibility.room_id IS NULL;
+    `);
+    return results.rows;
+}
