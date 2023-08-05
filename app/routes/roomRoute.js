f98e147b599d7a3a46d7e3456e69d34c0434a712
tanjonaaa <hei.tanjona@gmail.com>
2023-08-05 16:46:54 +0300

    fix: room delete route used to call the update function, fixed it

--- app/routes/roomRoute.js
+++ app/routes/roomRoute.js
@@ -6,7 +6,7 @@ export const roomRouter = express.Router();
 
 roomRouter.post('/insert', controller.insertRoom);
 roomRouter.put('/update', controller.updateRoom);
-roomRouter.delete('/delete', controller.updateRoom);
+roomRouter.delete('/delete', controller.deleteRoom);
 
 roomRouter.get('/hotel/:id', controller.getAllRoom);
 roomRouter.get('/:id', controller.getRoomById);
