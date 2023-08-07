58011b0db094bd770fbf0e7dfc8c9aedb1c997f9
tanjonaaa <hei.tanjona@gmail.com>
2023-08-07 18:25:30 +0300

    feat: added room_options fetch for a single room

--- app/controller/room_optionController.js
+++ app/controller/room_optionController.js
@@ -7,3 +7,11 @@ export const getAllRoomOptions = (req, res) => {
         res.status(500).json({ error: "Failed to fetch AllRoomoptions." });
     });
 }
+
+export const getAllOptionsOfARoom = (req, res) => {
+    model.allOptionsOfARoom(req.params.id).then((results) => {
+        res.status(200).json(results);
+    }).catch((e) => {
+        res.status(500).json({ error: "Failed to fetch all options of a room." });
+    });
+}
