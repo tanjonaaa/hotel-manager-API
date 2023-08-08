7e1d7f91312226045a20fcd5f31dec4631c22d74
tendryAxel <hei.tendry@gmail.com>
2023-08-08 09:58:01 +0300

    Feat : moch data for city

---
+++ migration/updateCityImg.sql
@@ -0,0 +1,5 @@
+UPDATE city SET photo = 'http://localhost:8000/img/city/1.jpg' WHERE id = 1; 
+UPDATE city SET photo = 'http://localhost:8000/img/city/2.jpg' WHERE id = 2; 
+UPDATE city SET photo = 'http://localhost:8000/img/city/4.jpg' WHERE id = 4; 
+UPDATE city SET photo = 'http://localhost:8000/img/city/3.jpg' WHERE id = 3; 
+UPDATE city SET photo = 'http://localhost:8000/img/city/5.jpg' WHERE id = 5; 
