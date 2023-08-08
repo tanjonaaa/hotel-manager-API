33436fe29cb864004831b3b1ef8fd99bf9079a91
tendryAxel <hei.tendry@gmail.com>
2023-08-08 11:33:33 +0300

    Fix : error in payement method

--- app/models/paymentMethodModel.js
+++ app/models/paymentMethodModel.js
@@ -3,12 +3,12 @@ import {pool} from "../../config/connection.js";
 
 //Returns a Promise containing all payment method
 export const allPaymentMethod = async () =>{
-    const results = await pool.query('SELECT * FROM reservation;');
+    const results = await pool.query('SELECT * FROM payment_method;');
     return results.rows;
 }
 
 //Returns a Promise containing the single payment method by its id
 export const onePaymentMethodById = async (id) => {
-    const results = await pool.query('SELECT * FROM reservation WHERE id = $1;', [id]);
+    const results = await pool.query('SELECT * FROM payment_method WHERE id = $1;', [id]);
     return results.rows;
 }
